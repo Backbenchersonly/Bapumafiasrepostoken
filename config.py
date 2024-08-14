ab361a0d02102e04d8bd72dcc6d4deaa586d9f3e
Bapumafiyas <154887259+Bapumafiyas@users.noreply.github.com>
2024-08-14 11:59:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,8 +31,8 @@ PORT = os.environ.get("PORT", "8080")
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://sunitverma080:mg0Pd4Pdf0UD0Ou8@merabot1.dcduprk.mongodb.net/?retryWrites=true&w=majority&appName=merabot1")
 DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 
-SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "publicearn.com")
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "oooiikkkkksnnja")
+SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "shortyfi.link")
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "6ea45e31941204014975566c1a788f0492935434")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 86400)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "True")
 TUT_VID = os.environ.get("TUT_VID","https://t.me/sux_Preview/15")
